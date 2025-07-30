8902521f24fa30ec7cd83e18cb5dc386c4702fb5
worknech <worknech@yandex.ru>
2025-07-30 14:35:02 +0300

    Создана функция get_log_image загрузки ссылки на изображение

--- dogs.py
+++ dogs.py
@@ -7,7 +7,7 @@ from io import BytesIO
 
 def show_image():
     """Загрузка изображения в метку"""
-    image_url = get_dog_image()
+    image_url: str | None = get_dog_image()
     if image_url:
         try:
             response = requests.get(image_url, stream=True)
@@ -15,14 +15,22 @@ def show_image():
             img_data = BytesIO(response.content)
             img = Image.open(img_data)
             img.thumbnail((300, 300))
+            img = ImageTk.PhotoImage(img)
             label.config(image=img)
             label.image = img
         except Exception as e:
-            mb.showerror("Ошибка", f'Возникла ошибка {e}')
+            mb.showerror("Ошибка", f'Возникла ошибка при запросе к API {e}')
 
 
 def get_dog_image():
-    pass
+    try:
+        response = requests.get('https://dog.ceo/api/breeds/image/random')
+        response.raise_for_status()
+        data = response.json()
+        return data['message']
+    except Exception as e:
+        mb.showerror("Ошибка", f'Возникла ошибка при загрузке изображения {e}')
+        return None
 
 
 root = Tk()
