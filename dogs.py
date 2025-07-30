e22b99dfcde235a4898127ce2c79a9fc925599b8
worknech <worknech@yandex.ru>
2025-07-30 16:09:16 +0300

    Добавлен новый виджет notebook для отображения каждой картинки в новой вкладке

--- dogs.py
+++ dogs.py
@@ -20,10 +20,12 @@ def show_image():
             img_size = (int(with_spinbox.get()), int(height_spinbox.get()))
             img.thumbnail((img_size))
             img = ImageTk.PhotoImage(img)
-            new_window = Toplevel(root)
-            new_window.title('Случайное изображение')
-            lb = ttk.Label(new_window, image=img)
-            lb.pack()
+            # new_window = Toplevel(root)
+            # new_window.title('Случайное изображение')
+            tab = ttk.Frame(notebook)
+            notebook.add(tab, text=f'Картинка №{notebook.index('end') + 1}')
+            lb = ttk.Label(tab, image=img)
+            lb.pack(padx=10, pady=10)
             lb.image = img
         except Exception as e:
             mb.showerror("Ошибка", f'Возникла ошибка при запросе к API {e}')
@@ -63,13 +65,18 @@ progress.pack(pady=10)
 with_label = ttk.Label(text='Ширина:')
 with_label.pack(side='left', padx=(10, 0))
 with_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
+with_spinbox.set(300)
 with_spinbox.pack(side='left', padx=(0, 10))
 
 height_label = ttk.Label(text='Высота:')
 height_label.pack(side='left', padx=(10, 0))
 height_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
+height_spinbox.set(300)
 height_spinbox.pack(side='left', padx=(0, 10))
 
-
+top_level_window = Toplevel(root)
+top_level_window.title('Изображения собачек')
+notebook = ttk.Notebook(top_level_window)
+notebook.pack(expand=True, fill='both', padx=10, pady=10)
 
 root.mainloop()
